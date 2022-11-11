8b4b29fc75fe52520150ec58919d69ff277b3869
Andres Garcia <pipeavs19@gmail.com>
2022-11-11 17:34:16 -0500

    Modificacion sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,13 +2,6 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    .titulo-sexto.color-acento-contenido.mb-5
-      .row.mb-2
-        h5 Desarrollo de aplicaciones web #[i full] stack
-      .row
-        span Construcción del informe de requisitos
-    
-    img(src="@/assets/curso/temas/sintesis-img-01.svg", alt="alt").imagen-sintesis-1
 
     p.mb-5.mt-4 El siguiente mapa integra los criterios y especificidades de los conocimientos expuestos en el presente componente formativo.
 
